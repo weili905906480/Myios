8be1e3993887f14990176e496a58ed4877608d90
weili <weili@weilimatoiMac.local>
2014-10-15 13:52:54 +0800

    Initial Commit

---
+++ git练习/appAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  appAppDelegate.h
+//  git练习
+//
+//  Created by weili on 14-10-15.
+//  Copyright (c) 2014年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface appAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ git练习/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  git练习
+//
+//  Created by weili on 14-10-15.
+//  Copyright (c) 2014年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "appAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([appAppDelegate class]));
+    }
+}
