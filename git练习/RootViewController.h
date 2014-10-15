55f2d1abf55d01411982cb86940dad4b386537f6
weili <weili@weilimatoiMac.local>
2014-10-15 16:44:21 +0800

    Initial commit

---
+++ git练习/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  git练习
+//
+//  Created by weili on 14-10-15.
+//  Copyright (c) 2014年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
---
+++ git练习/RootViewController.m
@@ -0,0 +1,63 @@
+//
+//  RootViewController.m
+//  git练习
+//
+//  Created by weili on 14-10-15.
+//  Copyright (c) 2014年 __MyCompanyName__. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@implementation RootViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+/*
+// Implement loadView to create a view hierarchy programmatically, without using a nib.
+- (void)loadView
+{
+}
+*/
+
+
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    UILabel *label=[[UILabel alloc]initWithFrame:CGRectMake(0, 0, 50, 50)];
+    [self.view addSubview:label];
+    
+}
+
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
